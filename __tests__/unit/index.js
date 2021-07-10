611ea8af54636f1f2abedf38929dd5868ea663e9
Patrick Lee Scott <pat@patscott.io>
2021-07-09 22:56:02 -0500

    feat: aggregate based config
    BREAKING CHANGE: config is now based on aggregates for cqrs/es systems
    
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- __tests__/unit/index.js
+++ __tests__/unit/index.js
@@ -2,50 +2,89 @@ import { knativebus } from 'index'
 
 jest.mock('axios')
 
+const testBusConfig = {
+  aggregates: {
+    example: {
+      commands: 'http://broker-ingress.knative-eventing.svc.cluster.local/test/example-commands',
+      events: 'http://broker-ingress.knative-eventing.svc.cluster.local/test/example-events'
+    }
+  },
+  source: 'tests'
+}
+
 describe('knativebus', () => {
   describe('send', () => {
     afterEach(() => {
       jest.clearAllMocks()
     })
 
-    it('should expose a send function once initialized', () => {
-      const bus = knativebus({})
+    it('should throw an error if misconfigured', () => {
+      try {
+        knativebus({})
+      } catch (err) {
+        expect(err).toEqual(new Error('aggregates are required'))
+      }
 
-      expect(bus.send).toBeDefined()
+      try {
+        knativebus({
+          aggregates: {}
+        })
+      } catch (err) {
+        expect(err).toEqual(new Error('aggregates must contain at least one aggregate key'))
+      }
+
+      try {
+        knativebus({
+          aggregates: {
+            example: {
+            }
+          }
+        })
+      } catch (err) {
+        expect(err).toEqual(new Error('source is required (what service is sending this message)'))
+      }
+
+      const bus = knativebus({
+        aggregates: {
+          example: {
+          }
+        },
+        source: 'tests'
+      })
+      expect(bus).toBeDefined()
+    })
+
+    it('should expose a send function once initialized', () => {
+      const bus = knativebus(testBusConfig)
+
       expect(typeof bus.send === 'function').toBe(true)
     })
 
-    it('send without broker', async () => {
+    it('should throw an error if trying to send without aggregate commands broker', async () => {
       const bus = knativebus({
-        brokers: {
+        aggregates: {
+          example: {
+          }
         },
-        source: 'knativebus-tests'
+        source: 'tests'
       })
 
       const axios = require('axios')
 
-      const command = 'example-aggregate.execute'
+      const command = 'example.execute'
       const data = { id: 1 }
 
       try {
         await bus.send(command, data)
       } catch (e) {
-        expect(e).toEqual(new Error('broker url has not been configured for example-aggregate'))
+        expect(e).toEqual(new Error('"example" commands broker url has not been configured'))
       }
       expect(axios).not.toBeCalled()
     })
 
     it('send invalid command', async () => {
-      const exampleAggregateModelBroker = 'http://broker-ingress.knative-eventing.svc.cluster.local/test/example-aggregate-model'
-      const bus = knativebus({
-        brokers: {
-          'example-aggregate': exampleAggregateModelBroker
-        },
-        source: 'knativebus-tests'
-      })
-
+      const bus = knativebus(testBusConfig)
       const axios = require('axios')
-
       const command = 'execute'
       const data = { id: 1 }
 
@@ -57,24 +96,29 @@ describe('knativebus', () => {
       expect(axios).not.toBeCalled()
     })
 
-    it('send', async () => {
-      const exampleAggregateModelBroker = 'http://broker-ingress.knative-eventing.svc.cluster.local/test/example-aggregate-model'
-      const bus = knativebus({
-        brokers: {
-          'example-aggregate': exampleAggregateModelBroker
-        },
-        source: 'knativebus-tests'
-      })
+    it('throws an error if aggregate has no config', () => {
+      const bus = knativebus(testBusConfig)
+      const command = 'turtle.crawl'
+      const data = { id: 1 }
 
+      try {
+        bus.send(command, data)
+      } catch (err) {
+        expect(err).toEqual(new Error('"turtle" has not been configured'))
+      }
+    })
+
+    it('send', async () => {
+      const bus = knativebus(testBusConfig)
       const axios = require('axios')
 
-      const command = 'example-aggregate.execute'
+      const command = 'example.execute'
       const data = { id: 1 }
 
       await bus.send(command, data)
       expect(axios).toBeCalledWith({
         method: 'post',
-        url: exampleAggregateModelBroker,
+        url: testBusConfig.aggregates.example.commands,
         data: JSON.stringify(data),
         headers: expect.any(Object)
       })
@@ -87,20 +131,13 @@ describe('knativebus', () => {
     })
 
     it('should expose a publish function once initialized', () => {
-      const bus = knativebus({})
+      const bus = knativebus(testBusConfig)
 
-      expect(bus.publish).toBeDefined()
       expect(typeof bus.publish === 'function').toBe(true)
     })
 
     it('publish invalid command', async () => {
-      const exampleAggregateModelBroker = 'http://broker-ingress.knative-eventing.svc.cluster.local/test/example-aggregate-model'
-      const bus = knativebus({
-        brokers: {
-          'example-aggregate': exampleAggregateModelBroker
-        },
-        source: 'knativebus-tests'
-      })
+      const bus = knativebus(testBusConfig)
 
       const axios = require('axios')
 
@@ -115,44 +152,52 @@ describe('knativebus', () => {
       expect(axios).not.toBeCalled()
     })
 
-    it('publish without channel', () => {
+    it('should throw an error if trying to send without aggregate events broker', async () => {
       const bus = knativebus({
-        channels: {
+        aggregates: {
+          example: {
+          }
         },
-        source: 'example-aggregate-model-tests'
+        source: 'tests'
       })
 
       const axios = require('axios')
 
-      const event = 'example-aggregate.event-happened'
+      const event = 'example.event-happened'
       const data = { id: 1 }
 
       try {
         bus.publish(event, data)
       } catch (e) {
-        expect(e).toEqual(new Error('channel url has not been configured for example-aggregate'))
+        expect(e).toEqual(new Error('"example" events broker url has not been configured'))
       }
       expect(axios).not.toBeCalled()
     })
 
+    it('throws an error if aggregate has no config', () => {
+      const bus = knativebus(testBusConfig)
+      const event = 'turtle.crawled'
+      const data = { id: 1 }
+
+      try {
+        bus.publish(event, data)
+      } catch (err) {
+        expect(err).toEqual(new Error('"turtle" has not been configured'))
+      }
+    })
+
     it('publish', () => {
-      const domainEvents = 'http://example-aggregate-domain-events-kn-channel.default.svc.cluster.local'
-      const bus = knativebus({
-        channels: {
-          'example-aggregate': domainEvents
-        },
-        source: 'example-aggregate-model-tests'
-      })
+      const bus = knativebus(testBusConfig)
 
       const axios = require('axios')
 
-      const event = 'example-aggregate.event-happened'
+      const event = 'example.event-happened'
       const data = { id: 1 }
 
       bus.publish(event, data)
       expect(axios).toBeCalledWith({
         method: 'post',
-        url: domainEvents,
+        url: testBusConfig.aggregates.example.events,
         data: JSON.stringify(data),
         headers: expect.any(Object)
       })
--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ const info = debug('knativebus')
 
 export const knativebus = (config) => {
   const {
-    brokers,
-    channels,
+    aggregates,
     source
   } = config
+
+  if (!aggregates) throw new Error('aggregates are required')
+  if (Object.keys(aggregates).length === 0) throw new Error('aggregates must contain at least one aggregate key')
+  if (!source) throw new Error('source is required (what service is sending this message)')
 
   return {
     publish: (type, data) => {
@@ -18,9 +21,12 @@ export const knativebus = (config) => {
       if (splitType.length < 2) throw new Error('format events as {aggregate}.{event-happened}')
 
       const model = splitType[0]
-      const modelDomainEventsChannel = channels[model]
+      const modelBusConfig = aggregates[model]
+      if (!modelBusConfig) throw new Error(`"${model}" has not been configured`)
 
-      if (!modelDomainEventsChannel) throw new Error(`channel url has not been configured for ${model}`)
+      const modelDomainEventsChannel = modelBusConfig.events
+
+      if (!modelDomainEventsChannel) throw new Error(`"${model}" events broker url has not been configured`)
       info('publishing event', {
         type, data, target: modelDomainEventsChannel
       })
@@ -47,9 +53,12 @@ export const knativebus = (config) => {
       if (splitType.length < 2) throw new Error('format commands as {aggregate}.{command}')
 
       const model = splitType[0]
-      const modelBroker = brokers[model]
+      const modelBusConfig = aggregates[model]
+      if (!modelBusConfig) throw new Error(`"${model}" has not been configured`)
 
-      if (!modelBroker) throw new Error(`broker url has not been configured for ${model}`)
+      const modelBroker = modelBusConfig.commands
+
+      if (!modelBroker) throw new Error(`"${model}" commands broker url has not been configured`)
 
       info('publishing command', {
         type, data, target: modelBroker
